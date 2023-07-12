cec4b2445dd374bdc31aad2f86ccfba577b00bf1
gzxmac <wanan499gzx@163.com>
2023-07-12 14:35:51 +0800

    feat: 删除一些无用代码

--- src/apis/alarm/alarm.api.ts
+++ src/apis/alarm/alarm.api.ts
@@ -2,31 +2,4 @@ import BaseApi from '../base.api';
 
 export default class AlarmApi extends BaseApi {
     private baseUri = '/be/assets';
-
-    queryTableDataDemo(params: { pageNum: number; pageSize: number }) {
-        // return this.request.get(`${this.baseUri}/page`, { params })
-        return Promise.resolve({
-            pageNum: 1,
-            pageSize: 10,
-            pages: 1,
-            rows: [
-                {
-                    id: '0',
-                    code: '001',
-                },
-                {
-                    id: '1',
-                    code: '001',
-                },
-                {
-                    id: '2',
-                    code: '001',
-                },
-                {
-                    id: '3',
-                    code: '001',
-                },
-            ],
-        });
-    }
 }
--- src/apis/assets/assets.api.ts
+++ src/apis/assets/assets.api.ts
@@ -3,48 +3,6 @@ import BaseApi from '../base.api';
 export default class AssetsApi extends BaseApi {
     private baseUri = '/be/asset';
 
-    queryTableDataDemo(params: { pageNum: number; pageSize: number }) {
-        // return this.request.get(`${this.baseUri}/page`, { params })
-        return Promise.resolve({
-            pageNum: 1,
-            pageSize: 10,
-            pages: 1,
-            rows: [
-                {
-                    id: '0',
-                    code: '001',
-                    name: '皮带机1',
-                    is_enabled: true,
-                    created_at: '3333',
-                    update_at: '4545445',
-                },
-                {
-                    id: '1',
-                    code: '001',
-                    name: '皮带机2',
-                    is_enabled: true,
-                    created_at: '3333',
-                    update_at: '4545445',
-                },
-                {
-                    id: '2',
-                    code: '001',
-                    name: '皮带机3',
-                    is_enabled: true,
-                    created_at: '3333',
-                    update_at: '4545445',
-                },
-                {
-                    id: '3',
-                    code: '001',
-                    name: '皮带机4',
-                    is_enabled: true,
-                    created_at: '3333',
-                    update_at: '4545445',
-                },
-            ],
-        });
-    }
     //生产设备
     /** 根据id查询生产设备 */
     getAssetById(id: string): Promise<any> {
