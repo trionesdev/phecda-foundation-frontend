cabad1da5a0630902c540ae3fbc7df45a2529524
gzxmac <wanan499gzx@163.com>
2023-07-12 14:20:05 +0800

    fix: 更新一些写法

--- src/views/monitoring-operations/event-response/detail/index.tsx
+++ src/views/monitoring-operations/event-response/detail/index.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import styles from './index.module.less';
 import { PageHeader, VPanel } from '@moensun/antd-react-ext';
 import { useNavigate, useParams } from 'react-router-dom';
+import { Button, Form, Space } from 'antd';
 
 const SceneDetail: React.FC = () => {
     const { id: typeCode } = useParams();
-
+    const [form] = Form.useForm();
     const navigate = useNavigate();
 
     const pageHelper = (
@@ -14,12 +15,21 @@ const SceneDetail: React.FC = () => {
             onBack={() => {
                 navigate(-1);
             }}
-        />
+            extra={
+                <Space>
+                    <Button type="primary">启用</Button>
+                    <Button type="primary">确定</Button>
+                    <Button type="primary">编辑</Button>
+                </Space>
+            }
+        ></PageHeader>
     );
     return (
-        <VPanel className={styles.wrapper} header={pageHelper}>
-            <div className={styles.contentWrapper}>场景详情</div>
-        </VPanel>
+        <Form form={form} className={styles.wrapper}>
+            <VPanel header={pageHelper}>
+                <div className={styles.contentWrapper}>场景详情</div>
+            </VPanel>
+        </Form>
     );
 };
 
