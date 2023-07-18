fe9f0bcbcb950d283f2e7e3c70fdd9d3b8a7ed2a
gzxmac <wanan499gzx@163.com>
2023-07-18 15:07:35 +0800

    feat: 场景动作的List

---
+++ src/views/monitoring-operations/event-response/detail/ActionsFormList.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+// import styles from './index.module.less';
+import { Button, Form, Input, Select, Space, Typography } from 'antd';
+import { NamePath } from 'antd/es/form/interface';
+import _ from 'lodash';
+import { useSceneContext } from '../components/SceneProvider';
+import { PlusOutlined } from '@ant-design/icons';
+
+type ActionsFormListType = {
+    // name: NamePath;
+};
+const ActionsFormList: React.FC<ActionsFormListType> = () => {
+    const { sceneForm } = useSceneContext();
+    const getActionsNamePath = (name: NamePath): NamePath => {
+        return _.concat('actions', name);
+    };
+    return (
+        <div>
+            <Form.List name="actions" initialValue={[{}]}>
+                {(conditionsFields, { add, remove }) => {
+                    return (
+                        <>
+                            {conditionsFields?.map(({ key, name }) => {
+                                return (
+                                    <div key={key}>
+                                        <Typography.Text strong>
+                                            场景动作{name + 1}
+                                        </Typography.Text>
+                                        <Space wrap>
+                                            <Form.Item
+                                                name="type"
+                                                label="场景动作类型"
+                                            >
+                                                <Select
+                                                    style={{ width: 180 }}
+                                                />
+                                            </Form.Item>
+                                            <Form.Item>
+                                                <Button
+                                                    type="link"
+                                                    danger
+                                                    size="small"
+                                                    onClick={() => {
+                                                        remove(name);
+                                                    }}
+                                                >
+                                                    删除
+                                                </Button>
+                                            </Form.Item>
+                                        </Space>
+                                    </div>
+                                );
+                            })}
+                            <Form.Item>
+                                <Button
+                                    disabled={conditionsFields.length >= 3}
+                                    type="link"
+                                    onClick={() => add()}
+                                    icon={<PlusOutlined />}
+                                >
+                                    新增场景动作
+                                </Button>
+                            </Form.Item>
+                        </>
+                    );
+                }}
+            </Form.List>
+        </div>
+    );
+};
+
+export default ActionsFormList;
--- src/views/monitoring-operations/event-response/detail/SceneDefinition.tsx
+++ src/views/monitoring-operations/event-response/detail/SceneDefinition.tsx
@@ -5,6 +5,7 @@ import { NamePath } from 'antd/es/form/interface';
 import _ from 'lodash';
 import { useSceneContext } from '../components/SceneProvider';
 import ConditionsFormList from './ConditionsFormList';
+import ActionsFormList from './ActionsFormList';
 
 type SceneDefinitionType = {
     // name: NamePath;
@@ -14,9 +15,13 @@ const SceneDefinition: React.FC<SceneDefinitionType> = () => {
     const getFilterConditionNamePath = (name: NamePath): NamePath => {
         return _.concat('filterCondition', name);
     };
+    const getActionsNamePath = (name: NamePath): NamePath => {
+        return _.concat('actions', name);
+    };
     return (
         <div>
             <Typography.Title level={3}>场景定义</Typography.Title>
+            {/* 发生事件 FilterCondition */}
             <Typography.Title level={5}>当以下事件发生</Typography.Title>
             <Space wrap>
                 <Form.Item
@@ -51,7 +56,12 @@ const SceneDefinition: React.FC<SceneDefinitionType> = () => {
                 </Form.Item>
             </Space>
             <Divider />
+            {/* 满足条件 conditions */}
             <ConditionsFormList />
+            <Divider />
+            {/* 场景动作 actions*/}
+            <Typography.Title level={3}>场景动作</Typography.Title>
+            <ActionsFormList />
         </div>
     );
 };
