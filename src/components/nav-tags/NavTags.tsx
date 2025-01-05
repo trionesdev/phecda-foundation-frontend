a4d0876e05b6451ab258a37375a56d77899b8de1
落雁沙 <fengxiaotx@163.com>
2025-01-05 19:41:49 +0800

    feat: demo update

--- src/components/nav-tags/NavTags.tsx
+++ src/components/nav-tags/NavTags.tsx
@@ -11,7 +11,7 @@ import classNames from 'classnames';
 
 type NavTagsProps = {}
 export const NavTags: FC<NavTagsProps> = ({}) => {
-    const params = useParams()
+    const params = useParams();
     const homeRoute = { id: 'home', label: '首页', path: () => '/' }; //此处的路由对象是自己设置的，要跟路由表对应上，由于匹配是根据ID的，记得路由配置要加上id
     const tagsMenuRef = useRef<any>();
     const tagsMenuWrapperRef = useRef<any>();
@@ -29,17 +29,17 @@ export const NavTags: FC<NavTagsProps> = ({}) => {
         } else {
             const matchRoute = routeMatch(routeId);
             if (matchRoute) {
-                setRouteObjects([...routeObjects, {...matchRoute,params}]);
+                setRouteObjects([...routeObjects, { ...matchRoute, params }]);
                 setActiveRouteObject(matchRoute);
             }
         }
     }, [matches]);
 
-    const prefixCls = `triones-nav-tags`
-    const {wrapSSR, hashId} = useCssInJs({prefix: prefixCls, styleFun: genNavTagsStyle})
+    const prefixCls = `triones-nav-tags`;
+    const { wrapSSR, hashId } = useCssInJs({ prefix: prefixCls, styleFun: genNavTagsStyle });
 
     return wrapSSR(
-        <div ref={tagsMenuRef} className={classNames(prefixCls,hashId)} onWheel={(e) => {
+        <div ref={tagsMenuRef} className={classNames(prefixCls, hashId)} onWheel={(e) => {
             const tagsMenuEl = tagsMenuRef.current;
             const tagsMenuWrapperEl = tagsMenuWrapperRef.current;
             if (tagsMenuEl.clientWidth >= tagsMenuWrapperEl.clientWidth) {
@@ -64,7 +64,7 @@ export const NavTags: FC<NavTagsProps> = ({}) => {
                 tagsMenuWrapperEl.style.transform = `translateX(${nextTranslateX}px)`;
             }
         }}>
-            <div ref={tagsMenuWrapperRef} className={classNames(`${prefixCls}-wrapper`,hashId)}>
+            <div ref={tagsMenuWrapperRef} className={classNames(`${prefixCls}-wrapper`, hashId)}>
                 {routeObjects.map((item: NavRouteObject) => <Tag key={item.id}
                                                                  className={item?.id === activeRouteObject?.id ? 'active-tag' : ''}
                                                                  icon={item.id == homeRoute.id ?
@@ -73,15 +73,17 @@ export const NavTags: FC<NavTagsProps> = ({}) => {
                                                                  onClose={() => {
                                                                      const newRouteObjects = routeObjects.filter(route => route.id !== item?.id);
                                                                      setRouteObjects(newRouteObjects);
-                                                                     let newRoute = null;
-                                                                     if (_.isEmpty(newRouteObjects)) {
-                                                                         newRoute = homeRoute;
-                                                                     } else {
-                                                                         newRoute = newRouteObjects[newRouteObjects.length - 1];
+                                                                     if (item.id === activeRouteObject?.id) {
+                                                                         let newRoute = null;
+                                                                         if (_.isEmpty(newRouteObjects)) {
+                                                                             newRoute = homeRoute;
+                                                                         } else {
+                                                                             newRoute = newRouteObjects[newRouteObjects.length - 1];
+                                                                         }
+                                                                         setActiveRouteObject(newRoute);
+                                                                         const path = _.isFunction(newRoute.path) ? newRoute.path() : newRoute.path;
+                                                                         navigate(generatePath(path!, item?.params));
                                                                      }
-                                                                     setActiveRouteObject(newRoute);
-                                                                     const path = _.isFunction(newRoute.path) ? newRoute.path() : newRoute.path;
-                                                                     navigate(path!);
                                                                  }}
                                                                  onClick={() => {
                                                                      const path = _.isFunction(item.path) ? item.path() : item.path;
@@ -89,6 +91,6 @@ export const NavTags: FC<NavTagsProps> = ({}) => {
                                                                  }}
                 >{item.label}</Tag>)}
             </div>
-        </div>
+        </div>,
     );
 };
